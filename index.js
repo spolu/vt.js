66ac262dd17e58f3ca4c5f3ce68472c6fbf0620f
Stanislas Polu <polu.stanislas@gmail.com>
2013-05-29 18:59:00 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+/*
+ * vt.js: vt.js
+ *
+ * Copyright (c) 2013, Stanislas Polu. All rights reserved.
+ * (see LICENSE file)
+ *
+ * @log
+ * - 20130529 @spolu    Creation
+ */
+'use strict';
+
+module.exports = require('./lib/vt');
+
